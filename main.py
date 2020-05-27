df3836652a8794dad3713b8f88c8da6265caf79e
umar <umarbasheer1996@gmail.com>
2020-05-27 21:54:01 +0530

    changes made

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 
-from gtts import gTTS
+#!/usr/bin/python
+import sys
+import os as OS
+from gtts import gTTS as voice
+import speech_recognition as sr
+sys.path.append('text2speech')
+sys.path.append('voice_recognize')
+import gt2s
+import voice_command
+#for gtts
+gt2s.speak(st,voice,OS)
+#for speech recognissation
+global voice
+voice=voice_command.get_audio(sr)
+print voice
--- text2speech/gt2s.py
+++ text2speech/gt2s.py
@@ -3,3 +3,4 @@ def speak(st,voice,OS):
     tts = voice(st)
     tts.save('file.mp3')
     OS.system("play file.mp3")
+    OS.system("rm -rf file.mp3")
