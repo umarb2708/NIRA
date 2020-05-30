30b81bd0bd9e9f8aa16acf899a700eb2851c9d47
umar <umarbasheer1996@gmail.com>
2020-05-30 13:25:41 +0530

    changes

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 #!/usr/bin/python
 import sys
 import os as OS
-from gtts import gTTS as voice
-import speech_recognition as sr
-from time import sleep as slp
-from ina219 import INA219 as bat_module
-from Adafruit_CharLCD import Adafruit_CharLCD
+from gtts import gTTS as voice#google text to speech
+import speech_recognition as sr #voice redognisation
+from time import sleep as slp #for delay
+from ina219 import INA219 as bat_module#battery status
+from Adafruit_CharLCD import Adafruit_CharLCD#lcd display
+import pyttsx3 as py_voice #ofline voice
 
 sys.path.append('text2speech')
 sys.path.append('voice_recognize')
@@ -15,6 +16,7 @@ sys.path.append('alert_system')
 import gt2s
 import voice_command
 import low_battery
+import pyt2s
 
 lcd = Adafruit_CharLCD(rs=21, en=20, d4=16, d5=12, d6=7, d7=8,
                        cols=16, lines=2)
@@ -30,6 +32,7 @@ class bat_status:
 obj1=bat_status()
 #for gtts
 gt2s.speak(st,voice,OS)
+pyt2syy.speak(st,py_voice)
 #for speech recognissation
 global voice
 voice=voice_command.get_audio(sr)
--- text2speech/pyttsx.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3 
-  
-# initialisation 
-engine = pyttsx3.init() 
-  
-# testing 
-engine.say("Hii iam Rex") 
-engine.say("Thank you, Geeksforgeeks") 
-engine.runAndWait()
