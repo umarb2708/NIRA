0fa130e34f90b760f14f237c3a1652d656558911
umar <umarbasheer1996@gmail.com>
2020-06-01 11:07:22 +0530

    new files

---
+++ date_time/date_and_time.py
@@ -0,0 +1,3 @@
+def say_date(dt):
+    x=dt.datetime.now()
+    return x
---
+++ wish_me/wishes.py
@@ -0,0 +1,8 @@
+def wish_me(hr):
+    if hr>'6' and hr <'12':
+        st="Good Morning."
+    elif hr>'12' and hr <'16':
+         st="Good Afternoon."
+    elif hr>'12' and hr <'16':
+         st="Good Evening."
+    return st
