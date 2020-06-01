d6f198976f9abb2492be3e1c7bb2fba533987954
umar <umarbasheer1996@gmail.com>
2020-06-01 11:07:17 +0530

    updated

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ from time import sleep as slp #for delay
 from ina219 import INA219 as bat_module#battery status
 from Adafruit_CharLCD import Adafruit_CharLCD#lcd display
 import pyttsx3 as py_voice #ofline voice
+import datetime as dt
 
 sys.path.append('text2speech')
 sys.path.append('voice_recognize')
 sys.path.append('alert_system')
+sys.path.append('date_time')
 
 import gt2s
 import voice_command
 import low_battery
 import pyt2s
+import date_and_time
 
-lcd = Adafruit_CharLCD(rs=21, en=20, d4=16, d5=12, d6=7, d7=8,
-                       cols=16, lines=2)
+#lcd = Adafruit_CharLCD(rs=21, en=20, d4=16, d5=12, d6=7, d7=8,cols=16, lines=2)
 
 
 class bat_status:
@@ -30,7 +32,16 @@ class bat_status:
         self.p=0
 
 obj1=bat_status()
+x=date_and_time.say_date(dt)
+print (x.strftime("%H:%M"))
+y=x.strftime("%H")
+if y<'12':
+    print "GM"
+    st="Hi. Good morning umar. How are you?"
+    pyt2syy.speak(st,py_voice)
+
 #for gtts
+"""""
 gt2s.speak(st,voice,OS)
 pyt2syy.speak(st,py_voice)
 #for speech recognissation
@@ -43,3 +54,4 @@ low_battery.get_battery_status(slp,bat_module,obj1)
 lcd.clear()
 lcd.message('{0:0.1f}V {1:0.1f}mA'.format(obj1.v, obj1.i))
 lcd.message('\n{0:0.1f} Watts'.format(obj1.p/1000))
+""""""
