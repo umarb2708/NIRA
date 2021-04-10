f987ff9ff790d303a14b86908dea88f4a128915e
Umar B <projects.innovize@gmail.com>
2021-04-10 12:36:02 +0530

    Added TTS

--- rex.py
+++ rex.py
@@ -1,4 +1,24 @@
 # REX Main Module
 #Designed and Developed By UMAR B
+import os
+import sys
+import pyttsx3
 
+
+
+#Text to Speech Initialisation
+engine = pyttsx3.init()
+#voices = engine.getProperty('voices')
+##engine.setProperty('voice',voices[0].id)
+#print("TTS initialisation complete with voice ID:"+voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    print("speaking:"+audio)
+    engine.runAndWait()
+
+#--------------------------------------------------------------
+#                     Main code Starts Here
+#--------------------------------------------------------------
+speak("Hi Umar. Good Night")
 
