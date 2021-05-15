414d24fe4d057448e5b9f2b4d504f34ad82b5bb4
Umar B <projects.innovize@gmail.com>
2021-05-15 18:59:06 +0530

    Train File added

--- database.py
+++ database.py
@@ -17,3 +17,11 @@ def insert_login_details(user,ip_address):
     mycursor.execute(sql, val)
     mydb.commit()
     print(mycursor.rowcount, "record inserted.")
+
+
+def insert_training_commands(values):
+    sql = "INSERT INTO commands (main_kw,opt_kw1,opt_kw2,opt_kw3,opt_kw4,opt_kw5,action_file,param1,param2,param3,param4,param5) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"
+    val = (values["main_kw"],values["opt_kw1"],values["opt_kw2"],values["opt_kw3"],values["opt_kw4"],values["opt_kw5"],values["action_file"],values["param1"],values["param2"],values["param3"],values["param4"],values["param5"])
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
--- rex.py
+++ rex.py
@@ -8,13 +8,14 @@
 # Rex is a python based Humanoid robot.
 #==============================================================================================================
 import import_file as f
-#import date_time as dt
+import train_rex as t
 wishme="dt.wishme()"
 
 f.db.insert_login_details(f.user.get_user(),f.user.get_Host_name_IP())
 f.tts.speak(eval("f."+wishme))
 f.tts.speak("i am rex the robot")
 f.tts.speak("please wait for initial configuration")
+#t.train_rex()
 
 #while 1:
     
