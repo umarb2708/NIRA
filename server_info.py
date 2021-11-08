f737faae9f4396a1d35a017fd63af6b02f93f410
Umar Basheer <umarbasheer@innovizesolutions.com>
2021-11-08 15:05:39 +0000

    Updated Hira info

--- server_info.py
+++ server_info.py
@@ -22,7 +22,10 @@ connected=0;
 values={
         "SNO":1
         }
-
+hira={
+        "id":1
+    }
+init_dne=0
 def get_cpu_temp():
     cmd="vcgencmd measure_temp"
     temp=os.popen(cmd).read()[:-1]
@@ -63,7 +66,24 @@ def server_details():
     values["up_time"]=str(get_uptime())
     values["connection"]=str(check_connectivity())
     #return values
-    
+
+def get_hira_details():
+    global init_dne
+    cmd="ps -aux | grep hira.py | grep 's+\|S+'"
+    out=os.popen(cmd).readline()[:-1]
+    if "/home/pi/HIRA/hira.py" in out:
+        t=out.split()
+        hira["user"]=str(t[0])
+        hira["pid"]=str(t[1])
+        hira["tty"]=str(t[6])
+        hira["status"]=str(1)
+        hira["init"]=str(1) if init_dne else str(0)
+        init_dne=1
+    else:
+        hira["pid"]=str(0)
+        hira["tty"]="pts/0"
+        hira["status"]=str(0)
+        hira["init"]=str(init_dne)
 def display_info():
     print("Temperature:"+str(get_cpu_temp()))
     print("Load:"+str(get_cpu_load())+"%")
@@ -75,10 +95,19 @@ def insert_raspi_info():
     sql="UPDATE `raspi_info` SET `temp` = '"+values["temp"]+"', `cpu_load` = '"+values["cpu_load"]+"', `used_ram` = '"+values["used_ram"]+"', `used_mem` = '"+values["used_mem"]+"', `up_time` = '"+values["up_time"]+"', `connection` = '"+values["connection"]+"' WHERE `raspi_info`.`id` = 1;"
     mycursor.execute(sql)
     mydb.commit()
-    print("Server info inserted")
+    print("Server info inserted->"+str(sql))
+
+def insert_hira_info():
+        sql="UPDATE `hira_info` SET `pid` = '"+hira["pid"]+"', `tty` = '"+hira["tty"]+"', `status` = '"+hira["status"]+"', `init` = '"+hira["init"]+"' WHERE `hira_info`.`id` = 1;"
+        mycursor.execute(sql)
+        mydb.commit()
+        print("Hira info inserted->"+str(sql))
+
 while 1:
     server_details()
     insert_raspi_info()
+    get_hira_details()
+    insert_hira_info()
     time.sleep(10)
 
 
