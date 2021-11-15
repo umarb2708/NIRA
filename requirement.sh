540cd3e3a2ad4e0b4586df77c36d6a95b7610a4f
Umar Basheer <umarbasheer@innovizesolutions.com>
2021-11-15 13:25:38 +0000

    Added new requirements

--- requirement.sh
+++ requirement.sh
@@ -31,6 +31,8 @@ pip3 install pure-python-adb
 
 #-------------Speech Recog Module--------------------------
 pip3 install SpeechRecognition
+sudo apt-get install portaudio19-dev
+pip3 install pyaudio
 
 #-------------Server Info Script---------------------------
 #This script should run in every boot: follow https://www.dexterindustries.com/howto/auto-run-python-programs-on-the-raspberry-pi 
