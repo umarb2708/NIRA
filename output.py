6384eedbf505d250b3490273597c52c72ddc94a6
Umar Basheer <umarbasheer@innovizesolutions.com>
2021-11-28 17:58:40 +0000

    update 0.2

--- output.py
+++ output.py
@@ -1,11 +1,16 @@
 import import_file as f
 
-def txt_out(str,mode):
+def txt_out(st,mode):
     mode_b=f.conv.decimalToBinary(mode)
-    if ser:
-        f.ser.ser_write(str)
-    if ter:
-        print(str)
-    if t2s:
-        f.tts.speak(str)
+
+    if mode_b[0]=='1':
+        #writing to tts
+        f.tts.speak(st)
+    if mode_b[1]=='1':
+        #writing to serial
+        f.ser.ser_write(st)
+    if mode_b[2]=='1':
+        #writing to terminal
+        print(st)
+
 
