3f7c694bd6c5cdd00a23b0842af18fb7aa388b2b
Umar Basheer <umarbasheer@innovizesolutions.com>
2021-11-28 17:58:40 +0000

    update 0.2

--- home_automation.py
+++ home_automation.py
@@ -1,28 +1,59 @@
 import import_file as f
-dev_num=0
-bulb_list=["bulb","light","lamp"]
+
+bulb_list=["bulb","light"]
+lamp_list=["lamp","led","L E D", "LED"]
+fan_list=["fan","FAN","F A N"]
 
 
 def turn_on_device(command):
-    print("Turning on light")
-    f.db.update_automation_table(find_device(command),1)
-    return "Device:"+str(find_device(command))+" turned on::OK"
+    dev=find_device(command)
+    f.db.update_automation_table(dev["dev_num"],1)
 
-def turn_off_dev(command):
-    print("Turning off Device")
-    f.db.update_automation_table(find_device(command),0)
-    return "Device:"+str(find_device(command))+" turned off::OK"
+    term_out="dev:"+dev["dev_name"]+"st:ON room:"+dev["room"]+" floor:"+dev["floor"]
+    f.out.txt_out(term_out,6)
+
+    say=dev["dev_name"]+" turned on ."
+    f.out.txt_out(say,1)
+    
+    f.db.insert_cmd_executed("turn on the "+dev["dev_name"],"1")
+
+def turn_off_device(command):
+    dev=find_device(command)
+    f.db.update_automation_table(dev["dev_num"],0)
+
+    term_out="dev:"+dev["dev_name"]+"st:OFF room:"+dev["room"]+" floor:"+dev["floor"]
+    f.out.txt_out(term_out,6)
+
+    say=dev["dev_name"]+" turned off ."
+    f.out.txt_out(say,1)
+
+    f.db.insert_cmd_executed("turn off the "+dev["dev_name"],"1")
 
 
 def find_device(command):
+    dev_det={
+            "dev_num"   :0,
+            "dev_name"  :"no device",
+            "room"      :"master bedroom",
+            "floor"     :"first"
+            }
+    #Checking bulb list in commands
+    for dev in bulb_list:
+        if dev in command and dev_det["dev_num"] == 0:
+            dev_det["dev_num"]  =4
+            dev_det["dev_name"] ="light"
+            break
 
-    if "light" in command:
-        dev_num=4
-    elif "fan" in command:
-            dev_num=5
-    elif "lamp" in command:
-            dev_num=6
-    else :
-            dev_num=7
-    return dev_num
+    for dev in fan_list :
+        if dev in command and dev_det["dev_num"] == 0:
+            dev_det["dev_num"]  =5
+            dev_det["dev_name"] ="fan"
+            break
+
+    for dev in lamp_list :
+        if dev in command and dev_det["dev_num"] == 0:
+            dev_det["dev_num"]  =6
+            dev_det["dev_name"] ="night lamp"
+            break
+    return dev_det
 
