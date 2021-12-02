6cca25e5cd5cdc23492a268faba1ec7ae4d437c2
Umar Basheer <umarbasheer@innovizesolutions.com>
2021-12-02 07:19:25 +0000

    Convert

--- number_convertion.py
+++ number_convertion.py
@@ -2,9 +2,12 @@
 
 def decimalToBinary(n):
     bi= bin(n).replace("0b", "")
-    bi="000"+bi
+    if len(bi) < 3:
+        for i in range(len(bi),4):
+            bi=bi+"0"
+    print("DEBUG::CONV->binary="+bi)
     return bi
 
 
-bi=decimalToBinary(3)
-print (bi[0])
+#bi=decimalToBinary(6)
+#print (bi[2])
