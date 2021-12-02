c9580ebe432cdf3163f75c4421493e54bf6f5f14
Umar Basheer <umarbasheer@innovizesolutions.com>
2021-12-02 07:19:25 +0000

    New way of output

--- output.py
+++ output.py
@@ -1,16 +1,16 @@
 import import_file as f
 
 def txt_out(st,mode):
-    mode_b=f.conv.decimalToBinary(mode)
-
-    if mode_b[0]=='1':
+    #mode_b=f.conv.decimalToBinary(mode)
+    
+    if mode[0]=='1':
         #writing to tts
         f.tts.speak(st)
-    if mode_b[1]=='1':
-        #writing to serial
-        f.ser.ser_write(st)
-    if mode_b[2]=='1':
+    if mode[1]=='1':
         #writing to terminal
         print(st)
+    if mode[2]=='1':
+        #writing to serial
+        f.ser.ser_write(st)
 
 
