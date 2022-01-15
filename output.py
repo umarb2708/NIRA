a3e6028b43f5f7a70bdf343d236b12d1bc1a0a8e
Umar Basheer <umarbasheer@innovizesolutions.com>
2022-01-15 06:51:52 +0000

    Updated

--- output.py
+++ output.py
@@ -1,4 +1,5 @@
 import import_file as f
+log_f=open('/home/pi/HIRA/output_log.log','w')
 
 def txt_out(st,mode):
     #mode_b=f.conv.decimalToBinary(mode)
@@ -8,7 +9,8 @@ def txt_out(st,mode):
         f.tts.speak(st)
     if mode[1]=='1':
         #writing to terminal
-        print(st)
+        log_f.write(st)
+
     if mode[2]=='1':
         #writing to serial
         f.ser.ser_write(st)
