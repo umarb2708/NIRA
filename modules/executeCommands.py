36e9463bf5803e194d951d8f0022372bdd04f535
Umar B <umar@innovize.in>
2022-12-30 18:08:46 +0530

    Added Execution and Update DB functions

--- modules/executeCommands.py
+++ modules/executeCommands.py
@@ -10,9 +10,9 @@
 #==============================================================================================================
 from modules import modulePkg as mPkg
 def startExecution():
-    lis_h=mPkg.db.SelectData("commands","*","WHERE priority = 'high'")
-    lis_m=mPkg.db.SelectData("commands","*","WHERE priority = 'med'")
-    lis_l=mPkg.db.SelectData("commands","*","WHERE priority = 'low'")
+    lis_h=mPkg.db.SelectData("commands","*","WHERE priority = 'high' AND exec = 0")#high priority list
+    lis_m=mPkg.db.SelectData("commands","*","WHERE priority = 'med' AND exec = 0")#Medium priority list
+    lis_l=mPkg.db.SelectData("commands","*","WHERE priority = 'low' AND exec = 0")#Low Priority List
 
     #print("High Priority List")
     #print(lis_h)
@@ -20,4 +20,38 @@ def startExecution():
     #print(lis_m)
     #print("Low Priority List")
     #print(lis_l)
-    
+
+    for row in lis_h:#Running High Priority list first
+        executeCmd(row)
+    for row in lis_m:#Running Medium Priority list then
+        executeCmd(row)
+    for row in lis_l:#Running Low Priority list last
+        executeCmd(row)
+
+
+    return 0
+
+def executeCmd(cmdDetails):
+    module=SearchKeyWord(cmdDetails[1])
+    print("Module:"+module)
+    UpdateCmdStatus(cmdDetails,0)
+
+
+
+def SearchKeyWord(command):
+    moduleFound=""
+    CCdata=mPkg.db.SelectData("command_centre","*","")
+    for row in CCdata:
+        if row[2] in command:
+            moduleFound=row[3]
+            break
+
+    return moduleFound
+def UpdateCmdStatus(cmdDetails,ErrStatus):
+    val={
+            "exec":1
+            }
+    CmdID=cmdDetails[0]
+    mPkg.db.UpdateData("commands",val,"id = "+str(CmdID))
+
+
