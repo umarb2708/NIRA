1866425b4566b797ed6dab8babc1d780075b6ed2
Umar Windows <umarbasheer1996@gmail.com>
2023-02-07 08:53:52 +0530

    Hira Wheels Code

---
+++ slaves/hiraWheels/auth.h
@@ -0,0 +1,5 @@
+const char* ssid = "Umar Jio";
+const char* password = "Umar@JIO123#";
+const char* serverHostname = "192.168.15.2";
+const String username = "hiraWheels";
+const String pass = "Esp@HIRA123#";
