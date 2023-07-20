7bb828664b1390b11160fd91839de56e73ed4004
Umar Basheer <umarbasheer@gmail.com>
2023-07-20 11:19:12 +0530

    V2_0

--- http_method.py
+++
@@ -1,7 +0,0 @@
-
-import import_file as f
-import requests
-
-def post(url,query_args):
-    x = requests.post(url, data = query_args)
-    return x.text
--- text2speech.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import os
-def speak(txt):
-    tts = gTTS(txt)
-    tts.save('hello.mp3')
-    cmd="omxplayer hello.mp3"
-    out=os.popen(cmd).read()[:-1]
